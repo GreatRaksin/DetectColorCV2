f485b933e4ce4c92a80fb5352ece27ad65fa3003
demidraksin <greatraksin@icloud.com>
2021-01-09 19:18:10 +0300

    Adding argument parser

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import cv2
 import numpy as np
 import pandas as pd
 import argparse
+
+'''Argument parser to take smth from terminal'''
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help='Image path')
+args = vars(ap.parse_args())
+img_path = args['image']
+
